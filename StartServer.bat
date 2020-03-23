c7ff5e749c1329b00391e6fcffba1d298d400759
Bartłomiej Aksman <35529114+Aksmano@users.noreply.github.com>
2020-03-23 17:01:27 +0100

    Update StartServer.bat

--- StartServer.bat
+++ StartServer.bat
@@ -3,5 +3,5 @@ echo.
 echo.
 echo Aby wylaczyc serwer wcisnij ctrl+c, po pojawieniu sie wpisz Y i nacisnij Enter
 echo.
-start chrome.exe http://localhost:3000
+start chrome.exe http://localhost:3010
 node ./server.js
